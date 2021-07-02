447bf1dacd6c75b8d98ef24d9005b6fc30f93a93
koh <kohbanye@gmail.com>
2021-07-02 19:56:34 +0900

    fix SetLight function

--- lus/solve.go
+++ lus/solve.go
@@ -25,33 +25,51 @@ func (b *Board) SetLight(row int, col int) error {
 
 	c.value = Light
 	c.canPut = false
+	// light the upper side
 	for i := 1; ; i++ {
-		update := false
-		if col-i >= 0 {
-			b.value[row][col-i].canPut = false
-			b.value[row][col-i].isBright = true
-			update = true
-		}
-		if col+i < b.xSize {
-			b.value[row][col+i].canPut = false
-			b.value[row][col+i].isBright = true
-			update = true
-		}
-		if row-i >= 0 {
-			b.value[row-i][col].canPut = false
-			b.value[row-i][col].isBright = true
-			update = true
-		}
-		if row+i < b.ySize {
-			b.value[row+i][col].canPut = false
-			b.value[row+i][col].isBright = true
-			update = true
-		}
-
-		if !update {
+		if col-i < 0 || b.value[row][col-i].isBlock() {
 			break
 		}
+		b.value[row][col-i].canPut = false
+		b.value[row][col-i].isBright = true
+			update = true
+	}
+	// light the under side
+	for i := 1; ; i++ {
+		if col+i >= b.xSize || b.value[row][col+i].isBlock() {
+			break
+		}
+		b.value[row][col+i].canPut = false
+		b.value[row][col+i].isBright = true
+			update = true
+	}
+	// light the left side
+	for i := 1; ; i++ {
+		if row-i < 0 || b.value[row-i][col].isBlock() {
+			break
+		}
+		b.value[row-i][col].canPut = false
+		b.value[row-i][col].isBright = true
+			update = true
+	}
+	// light the right side
+	for i := 1; ; i++ {
+		if row+i >= b.ySize || b.value[row+i][col].isBlock() {
+			break
+		}
+		b.value[row+i][col].canPut = false
+		b.value[row+i][col].isBright = true
+			update = true
 	}
 
 	return nil
+}
+
+func (c *Cell) isBlock() bool {
+	switch c.value {
+	case Block, Block0, Block1, Block2, Block3, Block4:
+		return true
+	default:
+		return false
+	}
 }
