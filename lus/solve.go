c09867096282261f5b0ea5188d23b59a5049296c
kohbanye <kohbanye@gmail.com>
2021-07-03 15:19:46 +0900

    fix divide solve.go file

--- lus/solve.go
+++ lus/solve.go
@@ -11,7 +11,7 @@ func Solve(b Board) (bool, Board) {
 		for i := 0; i < b.ySize; i++ {
 			for j := 0; j < b.xSize; j++ {
 				c := b.value[i][j]
-				if c.value == Space && c.canPut {
+				if c.canPut {
 					b_ := b
 					b.SetLight(i, j)
 
@@ -29,37 +29,6 @@ func Solve(b Board) (bool, Board) {
 		}
 		return false, b
 	}
-}
-
-func (b *Board) IsCorrect() bool {
-	for i := 0; i < b.ySize; i++ {
-		for j := 0; j < b.xSize; j++ {
-			c := b.value[i][j]
-
-			// check if all space cells are bright
-			if c.value == Space && !c.isBright {
-				return false
-			}
-
-			// check if numbers of lights around number-blocks are correct
-			diff := [4][2]int{{1, 0}, {-1, 0}, {0, 1}, {0, -1}}
-			if c.isNumberBlock() {
-				cnt := 0
-				for _, d := range diff {
-					if 0 <= i+d[0] && i+d[0] < b.ySize && 0 <= j+d[1] && j+d[1] < b.xSize &&
-						b.value[i+d[0]][j+d[1]].value == Light {
-						cnt++
-					}
-				}
-
-				if cnt != c.value {
-					return false
-				}
-			}
-		}
-	}
-
-	return true
 }
 
 func (b *Board) SetLight(row int, col int) error {
@@ -96,22 +65,4 @@ func (b *Board) SetLight(row int, col int) error {
 	}
 
 	return nil
-}
-
-func (c *Cell) isBlock() bool {
-	switch c.value {
-	case Block, Block0, Block1, Block2, Block3, Block4:
-		return true
-	default:
-		return false
-	}
-}
-
-func (c *Cell) isNumberBlock() bool {
-	switch c.value {
-	case Block0, Block1, Block2, Block3, Block4:
-		return true
-	default:
-		return false
-	}
 }
